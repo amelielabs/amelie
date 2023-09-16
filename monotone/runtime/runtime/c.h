49ee19cf3197eb4682f3de6200dfe4ab4b1c0e66
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-16 13:05:20 +0300

    runtime: add root include file and shared macroses

---
+++ monotone/runtime/runtime/c.h
@@ -0,0 +1,46 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <inttypes.h>
+#include <limits.h>
+#include <math.h>
+#include <setjmp.h>
+#include <assert.h>
+
+#include <pthread.h>
+
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <sys/eventfd.h>
+#include <sys/un.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <netdb.h>
