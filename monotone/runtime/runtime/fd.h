3a5cd0ecb5e8bc18b4b4a271cce178139ef18803
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-16 13:33:39 +0300

    runtime: add object for a file descriptor

---
+++ monotone/runtime/runtime/fd.h
@@ -0,0 +1,28 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Fd Fd;
+
+typedef void (*FdFunction)(Fd*);
+
+struct Fd
+{
+	int        fd;
+	int        mask;
+	FdFunction on_read;
+	void*      on_read_arg;
+	FdFunction on_write;
+	void*      on_write_arg;
+};
+
+static inline void
+fd_init(Fd* self)
+{
+	memset(self, 0, sizeof(*self));
+	self->fd = -1;
+}
