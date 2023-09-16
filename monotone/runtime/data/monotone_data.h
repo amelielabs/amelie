0f4a420c5b3e421aea0448aa470236c394078e6f
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-16 15:40:47 +0300

    data: update module header

--- monotone/runtime/data/monotone_data.h
+++ monotone/runtime/data/monotone_data.h
@@ -6,3 +6,15 @@
 // SQL OLTP database
 //
 
+// data format
+#include "data/type.h"
+#include "data/data.h"
+#include "data/data_op.h"
+#include "data/encode.h"
+
+// objects
+#include "data/make.h"
+
+// json
+#include "data/json.h"
+#include "data/to_string.h"
