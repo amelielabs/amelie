911c9dad14835f4862c118efc250d5ca2e8f69f1
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-16 15:04:56 +0300

    io: introduce basic logger

---
+++ monotone/runtime/io/logger.h
@@ -0,0 +1,23 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Logger Logger;
+
+struct Logger
+{
+	bool enable;
+	bool to_stdout;
+	int  fd;
+};
+
+void logger_init(Logger*);
+void logger_open(Logger*, const char*);
+void logger_set_enable(Logger*, bool enable);
+void logger_set_to_stdout(Logger*, bool enable);
+void logger_close(Logger*);
+void logger_write(void*, const char*, const char*, int, const char*, ...);
