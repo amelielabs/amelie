991e4507d7f96b990a83e9bfa0c5310473c222df
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-16 13:57:09 +0300

    runtime: add cross-task event condition object

---
+++ monotone/runtime/runtime/condition.h
@@ -0,0 +1,60 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Condition Condition;
+
+struct Condition
+{
+	Notify notify;
+	Event  event;
+	bool   attached;
+	List   link;
+};
+
+static inline void
+condition_init(Condition* self)
+{
+	self->attached = false;
+	event_init(&self->event);
+	notify_init(&self->notify);
+}
+
+static inline void
+condition_on_notify(void* arg)
+{
+	Condition* self = arg;
+	event_signal(&self->event);
+}
+
+static inline int
+condition_attach(Condition* self, Poller* poller)
+{
+	if (self->attached)
+		return 0;
+	int rc;
+	rc = notify_open(&self->notify, poller, condition_on_notify, self);
+	if (unlikely(rc == -1))
+		return -1;
+	self->attached = true;
+	return 0;
+}
+
+static inline void
+condition_detach(Condition* self)
+{
+	if (! self->attached)
+		return;
+	notify_close(&self->notify);
+	self->attached = false;
+}
+
+static inline void
+condition_signal(Condition* self)
+{
+	notify_signal(&self->notify);
+}
