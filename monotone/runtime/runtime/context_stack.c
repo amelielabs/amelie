28d3bb9d1581c40fb11f7df110063b8a775a3b35
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-16 13:42:53 +0300

    runtime: add execution context stack

---
+++ monotone/runtime/runtime/context_stack.c
@@ -0,0 +1,37 @@
+
+//
+// monotone
+//	
+// SQL OLTP database
+//
+
+#include <monotone_runtime.h>
+#include <valgrind/valgrind.h>
+
+void
+context_stack_init(ContextStack* self)
+{
+	self->size        = 0;
+	self->pointer     = 0;
+	self->valgrind_id = 0;
+}
+
+int
+context_stack_allocate(ContextStack* self, int size)
+{
+	self->size = size;
+	self->pointer = in_malloc_nothrow(size);
+	if (unlikely(self->pointer == NULL))
+		return -1;
+	self->valgrind_id =
+		VALGRIND_STACK_REGISTER(self->pointer, self->pointer + size);
+	return 0;
+}
+
+void
+context_stack_free(ContextStack* self)
+{
+	VALGRIND_STACK_DEREGISTER(self->valgrind_id);
+	if (self->pointer)
+		in_free(self->pointer);
+}
---
+++ monotone/runtime/runtime/context_stack.h
@@ -0,0 +1,20 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct ContextStack ContextStack;
+
+struct ContextStack
+{
+	char*  pointer;
+	size_t size;
+	int    valgrind_id;
+};
+
+void context_stack_init(ContextStack*);
+int  context_stack_allocate(ContextStack*, int);
+void context_stack_free(ContextStack*);
