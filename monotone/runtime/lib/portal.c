133048d40d1c6dbb47fb54d1f1c9509f487fbc32
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-16 16:07:22 +0300

    lib: introduce portal

---
+++ monotone/runtime/lib/portal.c
@@ -0,0 +1,25 @@
+
+//
+// monotone
+//	
+// SQL OLTP database
+//
+
+#include <monotone_runtime.h>
+#include <monotone_io.h>
+#include <monotone_data.h>
+#include <monotone_lib.h>
+
+void
+portal_to_channel(Portal* self, Buf* buf)
+{
+	Channel* dest = self->arg;
+	channel_write(dest, buf);
+}
+
+void
+portal_to_tcp(Portal* self, Buf* buf)
+{
+	Tcp* dest = self->arg;
+	tcp_send(dest, buf);
+}
---
+++ monotone/runtime/lib/portal.h
@@ -0,0 +1,33 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Portal Portal;
+
+typedef void (*PortalFunction)(Portal*, Buf*);
+
+struct Portal
+{
+	PortalFunction function;
+	void*          arg;
+};
+
+static inline void
+portal_init(Portal* self, PortalFunction function, void *arg)
+{
+	self->function = function;
+	self->arg      = arg;
+}
+
+static inline void
+portal_write(Portal* self, Buf* msg)
+{
+	self->function(self, msg);
+}
+
+void portal_to_channel(Portal*, Buf*);
+void portal_to_tcp(Portal*, Buf*);
