dc089fa4d5f7e6833500360538c7425bc4beeb01
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-16 13:30:10 +0300

    runtime: add timer manager

---
+++ monotone/runtime/runtime/timer_mgr.c
@@ -0,0 +1,136 @@
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+#include <monotone_runtime.h>
+
+void
+timer_mgr_init(TimerMgr* self)
+{
+	self->time_ms      = 0;
+	self->time_ns      = 0;
+	self->time_us      = 0;
+	self->time_cached  = false;
+	self->timers_count = 0;
+	self->timers_max   = 16;
+	self->timers_seq   = 0;
+	self->timers       = NULL;
+}
+
+void
+timer_mgr_free(TimerMgr* self)
+{
+	if (self->timers)
+		free(self->timers);
+}
+
+uint64_t
+timer_mgr_gettime(void)
+{
+	struct timespec t;
+	clock_gettime(CLOCK_MONOTONIC, &t);
+	return t.tv_sec*  (uint64_t) 1e9 + t.tv_nsec;
+}
+
+void
+timer_mgr_update(TimerMgr* self)
+{
+	if (self->time_cached)
+		return;
+	self->time_ns     = timer_mgr_gettime();
+	self->time_ms     = self->time_ns / 1000000;
+	self->time_us     = self->time_ns / 1000;
+	self->time_cached = true;
+}
+
+hot int
+timer_mgr_step(TimerMgr* self)
+{
+	if (self->timers_count == 0)
+		return 0;
+
+	int timers_hit = 0;
+	int i = 0;
+	for (; i < self->timers_count; i++)
+	{
+		Timer* timer = self->timers[i];
+		if (timer->timeout > self->time_ms)
+			break;
+		timer->function(timer);
+		timer->active = false;
+		timers_hit++;
+	}
+	if (! timers_hit)
+		return 0;
+	int timers_left = self->timers_count - timers_hit;
+	if (timers_left == 0)
+	{
+		self->timers_count = 0;
+		return timers_hit;
+	}
+
+	memmove(self->timers, self->timers + timers_hit, sizeof(Timer*) * timers_left);
+	self->timers_count -= timers_hit;
+	return timers_hit;
+}
+
+hot static int
+timer_mgr_cmp(const void* a_ptr, const void* b_ptr)
+{
+	auto a = *(Timer**)a_ptr;
+	auto b = *(Timer**)b_ptr;
+
+	if (a->timeout == b->timeout)
+		return (a->seq > b->seq) ? 1 : -1;
+
+	if (a->timeout > b->timeout)
+		return 1;
+
+	return -1;
+}
+
+hot int
+timer_mgr_add(TimerMgr* self, Timer* timer)
+{
+	int count = self->timers_count + 1;
+	if (self->timers == NULL || count >= self->timers_max)
+	{
+		int   timers_max = self->timers_max*  2;
+		void* timers = mn_realloc_nothrow(self->timers, timers_max*  sizeof(Timer*));
+		if (unlikely(timers == NULL))
+			return -1;
+		self->timers = timers;
+		self->timers_max = timers_max;
+	}
+
+	self->timers[count - 1] = timer;
+	timer->seq     = self->timers_seq++;
+	timer->timeout = self->time_ms + timer->interval;
+	timer->active  = true;
+	timer->clock   = clock;
+	qsort(self->timers, count, sizeof(Timer*), timer_mgr_cmp);
+	self->timers_count = count;
+	return 0;
+}
+
+hot void
+timer_mgr_remove(TimerMgr* self, Timer* timer)
+{
+	if (! timer->active)
+		return;
+	assert(self->timers_count >= 1);
+	int i = 0;
+	int j = 0;
+	for (; i < self->timers_count; i++)
+	{
+		if (self->timers[i] == timer)
+			continue;
+		self->timers[j] = self->timers[i];
+		j++;
+	}
+	timer->active = 0;
+	self->timers_count -= 1;
+}
---
+++ monotone/runtime/runtime/timer_mgr.h
@@ -0,0 +1,124 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Timer    Timer;
+typedef struct TimerMgr TimerMgr;
+
+typedef void (*TimerFunction)(Timer*);
+
+struct Timer
+{
+	bool          active;
+	uint64_t      seq;
+	uint64_t      timeout;
+	int           interval;
+	TimerFunction function;
+	void*         function_arg;
+	void*         clock;
+};
+
+struct TimerMgr
+{
+	uint64_t time_ms;
+	uint64_t time_us;
+	uint64_t time_ns;
+	bool     time_cached;
+	Timer**  timers;
+	int      timers_count;
+	int      timers_max;
+	uint64_t timers_seq;
+};
+
+void     timer_mgr_init(TimerMgr*);
+void     timer_mgr_free(TimerMgr*);
+void     timer_mgr_update(TimerMgr*);
+int      timer_mgr_step(TimerMgr*);
+int      timer_mgr_add(TimerMgr*, Timer*);
+void     timer_mgr_remove(TimerMgr*, Timer*);
+uint64_t timer_mgr_gettime(void);
+
+static inline void
+timer_mgr_reset(TimerMgr* self)
+{
+	self->time_cached = false;
+}
+
+static inline uint64_t
+timer_mgr_time_ns(TimerMgr* self)
+{
+	timer_mgr_update(self);
+	return self->time_ns;
+}
+
+static inline uint64_t
+timer_mgr_time_ms(TimerMgr* self)
+{
+	timer_mgr_update(self);
+	return self->time_ms;
+}
+
+static inline uint64_t
+timer_mgr_time_us(TimerMgr* self)
+{
+	timer_mgr_update(self);
+	return self->time_us;
+}
+
+static inline bool
+timer_mgr_has_active_timers(TimerMgr* self)
+{
+	return self->timers_count > 0;
+}
+
+static inline Timer*
+timer_mgr_timer_min(TimerMgr* self)
+{
+	if (self->timers_count == 0)
+		return NULL;
+	return self->timers[0];
+}
+
+static inline void
+timer_init(Timer* self, TimerFunction function, void* arg,
+           int interval)
+{
+	self->active       = false;
+	self->seq          = 0;
+	self->timeout      = 0;
+	self->interval     = interval;
+	self->function     = function;
+	self->function_arg = arg;
+	self->clock        = NULL;
+}
+
+static inline int
+timer_start(TimerMgr* self, Timer* timer)
+{
+	timer_mgr_update(self);
+	return timer_mgr_add(self, timer);
+}
+
+static inline void
+timer_stop(Timer* self)
+{
+	if (! self->active)
+		return;
+	timer_mgr_remove(self->clock, self);
+}
+
+static inline void
+time_start(uint64_t* time_us)
+{
+	*time_us = timer_mgr_gettime();
+}
+
+static inline void
+time_end(uint64_t* time_us)
+{
+	*time_us = (timer_mgr_gettime() -* time_us) / 1000;
+}
