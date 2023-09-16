06fc0b7a62111fa3aba5c2a363fa7941dabee943
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-16 15:17:41 +0300

    io: update module header

--- monotone/runtime/io/monotone_io.h
+++ monotone/runtime/io/monotone_io.h
@@ -6,3 +6,26 @@
 // SQL OLTP database
 //
 
+// file io
+#include "io/iov.h"
+#include "io/vfs.h"
+#include "io/fs.h"
+#include "io/file.h"
+#include "io/logger.h"
+
+// blob
+#include "io/blob.h"
+
+// tls
+#include "io/tls_lib.h"
+#include "io/tls_context.h"
+#include "io/tls.h"
+
+// network io
+#include "io/socket.h"
+#include "io/poll.h"
+#include "io/tcp.h"
+#include "io/listen.h"
+
+// resolver
+#include "io/resolver.h"
