56122c8c03111ddf3f89a9644ae52a6801e1a2cd
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-16 16:00:53 +0300

    lib: implement uri parser

---
+++ monotone/runtime/lib/uri.h
@@ -0,0 +1,51 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct UriHost UriHost;
+typedef struct UriArg  UriArg;
+typedef struct Uri     Uri;
+
+typedef enum
+{
+	URI_PROTO_DEFAULT,
+	URI_PROTO_MOCK
+} UriProto;
+
+struct UriHost
+{
+	Str  host;
+	int  port;
+	List link;
+};
+
+struct UriArg
+{
+	Str  name;
+	Str  value;
+	List link;
+};
+
+struct Uri
+{
+	UriProto proto;
+	Str      user;
+	Str      password;
+	Str      db;
+	Str      uri;
+	int      hosts_count;
+	List     hosts;
+	int      args_count;
+	List     args;
+	char*    pos;
+};
+
+void uri_init(Uri*);
+void uri_free(Uri*);
+void uri_set(Uri*, bool, Str*);
+UriArg*
+uri_find(Uri*, const char*, int);
