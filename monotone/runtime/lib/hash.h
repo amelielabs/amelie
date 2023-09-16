384aad4494dde8de82332de836b68ba8ae4bd86d
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-16 15:53:14 +0300

    lib: add FNV and MURMUR3 hash functions

---
+++ monotone/runtime/lib/hash.h
@@ -0,0 +1,62 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+static inline uint32_t
+hash_fnv(const char *data, int size)
+{
+	const unsigned char *pos = (const unsigned char*)data;
+	const unsigned char *end = pos + size;
+	uint32_t hash = 2166136261;
+	while (pos < end)
+	{
+		hash = (hash * 16777619) ^ *pos;
+		pos++;
+	}
+	return hash;
+}
+
+hot static inline uint32_t
+hash_murmur_scramble(uint32_t k)
+{
+	k *= 0xcc9e2d51;
+	k = (k << 15) | (k >> 17);
+	k *= 0x1b873593;
+	return k;
+}
+
+hot static inline uint32_t
+hash_murmur3_32(const uint8_t* key, size_t len, uint32_t seed)
+{
+	uint32_t h = seed;
+	uint32_t k;
+
+	for (size_t i = len >> 2; i; i--)
+	{
+		memcpy(&k, key, sizeof(uint32_t));
+		key += sizeof(uint32_t);
+		h ^= hash_murmur_scramble(k);
+		h = (h << 13) | (h >> 19);
+		h = h * 5 + 0xe6546b64;
+	}
+
+	k = 0;
+	for (size_t i = len & 3; i; i--)
+	{
+		k <<= 8;
+		k |= key[i - 1];
+	}
+
+	h ^= hash_murmur_scramble(k);
+	h ^= len;
+	h ^= h >> 16;
+	h *= 0x85ebca6b;
+	h ^= h >> 13;
+	h *= 0xc2b2ae35;
+	h ^= h >> 16;
+	return h;
+}
