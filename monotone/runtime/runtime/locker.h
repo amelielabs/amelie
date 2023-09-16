19389d5c8ef293c15fd547eea65a2e86ea8d3b9d
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-16 14:12:28 +0300

    runtime: add locker object

---
+++ monotone/runtime/runtime/locker.h
@@ -0,0 +1,83 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Locker      Locker;
+typedef struct LockerCache LockerCache;
+
+struct Locker
+{
+	int        refs;
+	void*      lock;
+	bool       shared;
+	Coroutine* coroutine;
+	List       link;
+};
+
+struct LockerCache
+{
+	List list;
+	int  list_count;
+};
+
+static inline void
+locker_cache_init(LockerCache* self)
+{
+	self->list_count = 0;
+	list_init(&self->list);
+}
+
+static inline void
+locker_cache_free(LockerCache* self)
+{
+	list_foreach_safe(&self->list)
+	{
+		auto locker = list_at(Locker, link);
+		mn_free(locker);
+	}
+}
+
+static inline Locker*
+locker_cache_pop(LockerCache* self)
+{
+	Locker* locker = NULL;
+	if (self->list_count > 0)
+	{
+		auto first = list_pop(&self->list);
+		locker = container_of(first, Locker, link);
+		self->list_count--;
+	} else
+	{
+		locker = mn_malloc(sizeof(Locker));
+		list_init(&locker->link);
+	}
+	locker->refs      = 0;
+	locker->lock      = NULL;
+	locker->shared    = false;
+	locker->coroutine = mn_self();
+	return locker;
+}
+
+static inline void
+locker_cache_push(LockerCache* self, Locker* locker)
+{
+	locker->refs      = 0;
+	locker->lock      = NULL;
+	locker->shared    = false;
+	locker->coroutine = NULL;
+	self->list_count++;
+	list_append(&self->list, &locker->link);
+}
+
+static inline Locker*
+locker_create(LockerCache* self, void* lock, bool shared)
+{
+	auto locker = locker_cache_pop(self);
+	locker->lock   = lock;
+	locker->shared = shared;
+	return locker;
+}
