6d7d40c130620558ed70537c1f4d229bcac3bbb9
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-16 16:07:42 +0300

    lib: update module header

--- monotone/runtime/lib/monotone_lib.h
+++ monotone/runtime/lib/monotone_lib.h
@@ -6,3 +6,26 @@
 // SQL OLTP database
 //
 
+// data structures
+#include "lib/rbtree.h"
+#include "lib/hash_table.h"
+
+// snapshot manager
+#include "lib/snapshot_mgr.h"
+
+// hashing
+#include "lib/hash.h"
+#include "lib/sha1.h"
+#include "lib/crc.h"
+#include "lib/uuid_mgr.h"
+
+// compression
+#include "lib/compression.h"
+
+// parsers
+#include "lib/uri.h"
+#include "lib/lex.h"
+
+// messaging
+#include "lib/request.h"
+#include "lib/portal.h"
