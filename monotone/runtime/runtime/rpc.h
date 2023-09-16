2a3f782d6b316c16c89b687bafdab2f45714b74b
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-16 14:10:57 +0300

    runtime: add support for cross-task rpc

---
+++ monotone/runtime/runtime/rpc.h
@@ -0,0 +1,97 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Rpc Rpc;
+
+struct Rpc
+{
+	int        id;
+	int        argc;
+	intptr_t*  argv;
+	int        rc;
+	Error*     error;
+	Condition* cond;
+};
+
+always_inline hot static inline Rpc*
+rpc_of(Buf* buf)
+{
+	return *(Rpc**)msg_of(buf)->data;
+}
+
+always_inline hot static inline intptr_t
+rpc_arg(Rpc* rpc, int pos)
+{
+	return rpc->argv[pos];
+}
+
+always_inline hot static inline void*
+rpc_arg_ptr(Rpc* rpc, int pos)
+{
+	return (void*)rpc->argv[pos];
+}
+
+always_inline hot static inline void
+rpc_done(Rpc* rpc)
+{
+	condition_signal(rpc->cond);
+}
+
+hot static inline void
+rpc_execute(Buf* buf,
+            void (*callback)(Rpc*, void*),
+            void  *callback_arg)
+{
+	auto rpc = rpc_of(buf);
+	Exception e;
+	if (try(&e))
+		callback(rpc, callback_arg);
+	if (unlikely(catch(&e)))
+		*rpc->error = mn_self()->error;
+	rpc_done(rpc);
+}
+
+hot static inline int
+rpc(Channel* channel, int id, int argc, ...)
+{
+	// prepare arguments
+	intptr_t argv[argc];
+	va_list args;
+	va_start(args, argc);
+	int i = 0;
+	for (; i < argc; i++)
+		argv[i] = va_arg(args, intptr_t);
+	va_end(args);
+
+	// prepare condition
+	mn_self()->error.code = ERROR_NONE;
+	Rpc rpc =
+	{
+		.id    = id,
+		.argc  = argc,
+		.argv  = argv,
+		.rc    = 0,
+		.error = &mn_self()->error,
+		.cond  = condition_create()
+	};
+	Rpc *rpc_ptr = &rpc;
+
+	// do rpc call and wait for completion
+	auto buf = msg_create(id);
+	buf_write(buf, &rpc_ptr, sizeof(void**));
+	msg_end(buf);
+	channel_write(channel, buf);
+
+	condition_wait(rpc.cond, -1);
+	condition_free(rpc.cond);
+
+	if (mn_self()->error.code != ERROR_NONE)
+		rethrow();
+
+	return rpc.rc;
+}
