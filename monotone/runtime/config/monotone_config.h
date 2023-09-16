bcd400620b1bd04cff280b30cd6272aa616acd9a
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-16 16:23:04 +0300

    config: update module header

--- monotone/runtime/config/monotone_config.h
+++ monotone/runtime/config/monotone_config.h
@@ -6,3 +6,15 @@
 // SQL OLTP database
 //
 
+// config
+#include "config/var.h"
+#include "config/config.h"
+#include "config/config_state.h"
+#include "config/control.h"
+
+// catalog
+#include "config/catalog.h"
+#include "config/catalog_mgr.h"
+
+// global
+#include "config/global.h"
