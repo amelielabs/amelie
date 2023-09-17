46311ea0ab852da537756eb61c46c3ec7ee07066
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-17 10:23:46 +0300

    db: fix spacing

--- monotone/db/db/table_mgr.h
+++ monotone/db/db/table_mgr.h
@@ -20,7 +20,6 @@ void   table_mgr_gc(TableMgr*, uint64_t);
 void   table_mgr_create(TableMgr*, Transaction*, TableConfig*, bool);
 void   table_mgr_drop(TableMgr*, Transaction*, Str*, bool);
 void   table_mgr_alter(TableMgr*, Transaction*, Str*, TableConfig*, bool);
-
 Table* table_mgr_find(TableMgr*, Str*, bool);
 Table* table_mgr_find_by_id(TableMgr*, Uuid*);
 Buf*   table_mgr_show(TableMgr*);
