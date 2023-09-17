fbf32451e8b7db306fb24945d75af54342046733
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-17 10:21:49 +0300

    db: update module header

--- monotone/db/db/monotone_db.h
+++ monotone/db/db/monotone_db.h
@@ -6,3 +6,19 @@
 // SQL OLTP database
 //
 
+// table
+#include "db/table_config.h"
+#include "db/table.h"
+#include "db/table_op.h"
+#include "db/table_mgr.h"
+
+// meta
+#include "db/meta_config.h"
+#include "db/meta.h"
+#include "db/meta_op.h"
+#include "db/meta_mgr.h"
+
+// db
+#include "db/db.h"
+#include "db/db_catalog.h"
+#include "db/recover.h"
