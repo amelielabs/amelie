26b94edd8c3f9f0f348bb6a78adebe1fa4dcf98b
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-17 10:07:11 +0300

    wal: update module header

--- monotone/db/wal/monotone_wal.h
+++ monotone/db/wal/monotone_wal.h
@@ -6,3 +6,12 @@
 // SQL OLTP database
 //
 
+#include "wal/wal_record.h"
+#include "wal/wal_file.h"
+#include "wal/wal_snapshot.h"
+#include "wal/wal_event.h"
+#include "wal/wal_store.h"
+#include "wal/wal_store_open.h"
+#include "wal/wal.h"
+#include "wal/wal_cursor.h"
+#include "wal/debug.h"
