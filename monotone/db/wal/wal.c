be601a4f6c148fe0200c6800015abfc130636bc3
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-17 10:05:21 +0300

    wal: add wal task

---
+++ monotone/db/wal/wal.c
@@ -0,0 +1,132 @@
+
+//
+// monotone
+//	
+// SQL OLTP database
+//
+
+#include <monotone_runtime.h>
+#include <monotone_io.h>
+#include <monotone_data.h>
+#include <monotone_lib.h>
+#include <monotone_config.h>
+#include <monotone_schema.h>
+#include <monotone_mvcc.h>
+#include <monotone_engine.h>
+#include <monotone_storage.h>
+#include <monotone_wal.h>
+
+hot static void
+wal_rpc(Rpc* rpc, void* arg)
+{
+	unused(rpc);
+	Wal* self = arg;
+	switch (rpc->id) {
+	case RPC_WAL_GC:
+	{
+		uint64_t snapshot = rpc_arg(rpc, 0);
+		wal_store_gc(&self->wal_store, snapshot);
+		break;
+	}
+	case RPC_WAL_SNAPSHOT:
+	{
+		WalSnapshot* snapshot = rpc_arg_ptr(rpc, 0);
+		wal_store_snapshot(&self->wal_store, snapshot);
+		break;
+	}
+	case RPC_WAL_WRITE:
+	{
+		Log* log = rpc_arg_ptr(rpc, 0);
+		wal_store_write(&self->wal_store, log);
+		break;
+	}
+	case RPC_WAL_STATUS:
+	{
+		Buf** buf = rpc_arg_ptr(rpc, 0);
+		*buf = wal_store_status(&self->wal_store);
+		break;
+	}
+	default:
+		break;
+	}
+}
+
+static void
+wal_main(void *arg)
+{
+	Wal* self = arg;
+	bool stop = false;
+	while (! stop)
+	{
+		auto buf = channel_read(&mn_task->channel, -1);
+		auto msg = msg_of(buf);
+
+		// command
+		stop = msg->id == RPC_STOP;
+		rpc_execute(buf, wal_rpc, self);
+		buf_free(buf);
+	}
+}
+
+void
+wal_init(Wal* self)
+{
+	wal_store_init(&self->wal_store);
+	task_init(&self->task, mn_task->buf_cache);
+}
+
+void
+wal_free(Wal* self)
+{
+	wal_store_free(&self->wal_store);
+}
+
+void
+wal_open(Wal* self)
+{
+	wal_store_open(&self->wal_store);
+}
+
+void
+wal_start(Wal* self)
+{
+	task_create(&self->task, "wal", wal_main, self);
+}
+
+void
+wal_stop(Wal* self)
+{
+	if (task_active(&self->task))
+	{
+		rpc(&self->task.channel, RPC_STOP, 0);
+		task_wait(&self->task);
+		task_free(&self->task);
+		task_init(&self->task, mn_task->buf_cache);
+	}
+}
+
+void
+wal_gc(Wal* self, uint64_t snapshot)
+{
+	rpc(&self->task.channel, RPC_WAL_GC, 1, snapshot);
+}
+
+void
+wal_snapshot(Wal* self, WalSnapshot* snapshot)
+{
+	rpc(&self->task.channel, RPC_WAL_SNAPSHOT, 1, snapshot);
+}
+
+void
+wal_write(Wal* self, Log* log)
+{
+	rpc(&self->task.channel, RPC_WAL_WRITE, 1, log);
+}
+
+Buf*
+wal_status(Wal* self)
+{
+	Buf* buf = NULL;
+	rpc(&self->task.channel, RPC_WAL_STATUS, 1, &buf);
+	return buf;
+}
---
+++ monotone/db/wal/wal.h
@@ -0,0 +1,25 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Wal Wal;
+
+struct Wal
+{
+	WalStore wal_store;
+	Task     task;
+};
+
+void wal_init(Wal*);
+void wal_free(Wal*);
+void wal_open(Wal*);
+void wal_start(Wal*); 
+void wal_stop(Wal*); 
+void wal_gc(Wal*, uint64_t);
+void wal_snapshot(Wal*, WalSnapshot*);
+void wal_write(Wal*, Log*);
+Buf* wal_status(Wal*);
