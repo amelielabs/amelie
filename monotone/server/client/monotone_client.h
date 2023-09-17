94be44aca5fb36aee11cc3472dfb9792347e914c
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-17 08:55:06 +0300

    client: update module header

--- monotone/server/client/monotone_client.h
+++ monotone/server/client/monotone_client.h
@@ -6,3 +6,13 @@
 // SQL OLTP database
 //
 
+// connection
+#include "client/access.h"
+#include "client/connection.h"
+#include "client/connection_mgr.h"
+#include "client/connection_pool.h"
+#include "client/relay.h"
+
+// client
+#include "client/client.h"
+#include "client/client_mgr.h"
