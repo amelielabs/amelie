b1abbb2a4d40be33fa2aabf854636549251bed74
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-20 13:55:12 +0300

    server: update module header

--- monotone/server/server/monotone_server.h
+++ monotone/server/server/monotone_server.h
@@ -6,3 +6,11 @@
 // SQL OLTP database
 //
 
+// hub
+#include "server/hub.h"
+#include "server/hub_mgr.h"
+
+// server
+#include "server/server_config.h"
+#include "server/server_listen.h"
+#include "server/server.h"
