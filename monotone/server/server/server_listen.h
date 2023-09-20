379128a48af7b13b8a3e871d388caaf2611dd8dd
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-20 13:54:14 +0300

    server: add listen object

---
+++ monotone/server/server/server_listen.h
@@ -0,0 +1,43 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct ServerListen ServerListen;
+
+struct ServerListen
+{
+	Listen           listen;
+	uint64_t         worker;
+	struct addrinfo* addr;
+	bool             tls;
+	TlsContext*      tls_context;
+	ServerConfig*    config;
+	void*            arg;
+	List             link;
+};
+
+static inline ServerListen*
+server_listen_allocate(ServerConfig* config)
+{
+	ServerListen* self;
+	self = (ServerListen*)mn_malloc(sizeof(*self));
+	self->worker      = UINT64_MAX;
+	self->addr        = NULL;
+	self->tls         = false;
+	self->tls_context = NULL;
+	self->config      = config;
+	self->arg         = NULL;
+	listen_init(&self->listen);
+	list_init(&self->link);
+	return self;
+}
+
+static inline void
+server_listen_free(ServerListen* self)
+{
+	mn_free(self);
+}
