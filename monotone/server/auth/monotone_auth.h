f413f9ecdef5a53a863ddb2d65f7a3cecd0182d5
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-20 13:20:31 +0300

    auth: update module header

--- monotone/server/auth/monotone_auth.h
+++ monotone/server/auth/monotone_auth.h
@@ -6,7 +6,11 @@
 // SQL OLTP database
 //
 
+// user
 #include "auth/user_config.h"
 #include "auth/user.h"
+#include "auth/user_cache.h"
 #include "auth/user_mgr.h"
+
+// authentication
 #include "auth/auth.h"
