a0060a7522a8ca80da0872ed3871ad458ca0a1db
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-20 15:04:48 +0300

    api: add module

---
+++ monotone/main/api/monotone_internal.h
@@ -0,0 +1,19 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+#include <monotone_runtime.h>
+#include <monotone_io.h>
+#include <monotone_data.h>
+#include <monotone_lib.h>
+#include <monotone_config.h>
+#include <monotone_auth.h>
+#include <monotone_client.h>
+#include <monotone_server.h>
+#include <monotone_session.h>
+#include <monotone_core.h>
+#include <monotone_main.h>
