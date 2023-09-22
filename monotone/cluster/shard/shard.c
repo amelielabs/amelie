298e6ef569c87ef403b4dc0f6992a6b4e8abd361
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-22 11:42:09 +0300

    shard: introducee shard module

---
+++ monotone/cluster/shard/shard.c
@@ -0,0 +1,16 @@
+
+//
+// monotone
+//	
+// SQL OLTP database
+//
+
+#include <monotone_runtime.h>
+#include <monotone_io.h>
+#include <monotone_data.h>
+#include <monotone_lib.h>
+#include <monotone_config.h>
+#include <monotone_auth.h>
+#include <monotone_client.h>
+#include <monotone_server.h>
+#include <monotone_shard.h>
