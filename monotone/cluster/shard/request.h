48229e32541a3ceab89fa17c3cb4fffb8afcf8ca
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-23 12:52:02 +0300

    shard: add request functions

--- monotone/cluster/shard/request.h
+++ monotone/cluster/shard/request.h
@@ -10,17 +10,58 @@ typedef struct Request Request;
 
 struct Request
 {
+	bool       complete;
 	bool       ro;
-	Condition* on_commit; // set after ready for !ro
+	Condition* on_commit;
 	// code
 	// transaction
-	Channel*   portal;
+	Channel*   dst;
+	Channel    src;
+	Buf*       error;
+	List       link;
 };
 
-static inline void
-request_init(Request* self)
+always_inline hot static inline Request*
+request_of(Buf* buf)
 {
+	return *(Request**)msg_of(buf)->data;
+}
+
+static inline Request*
+request_allocate(void)
+{
+	Request* self = mn_malloc(sizeof(*self));
+	self->complete  = false;
 	self->ro        = false;
 	self->on_commit = NULL;
-	self->portal    = NULL;
+	self->dst       = NULL;
+	self->error     = NULL;
+	channel_init(&self->src);
+	list_init(&self->link);
+	return self;
+}
+
+static inline void
+request_free(Request* self)
+{
+	if (self->error)
+		buf_free(self->error);
+	channel_detach(&self->src);
+	channel_free(&self->src, mn_task->buf_cache);
+	mn_free(self);
+}
+
+static inline void
+request_reset(Request* self)
+{
+	if (self->error)
+	{
+		buf_free(self->error);
+		self->error = NULL;
+	}
+	self->complete   = false;
+	self->ro         = false;
+	self->on_commit  = NULL;
+	self->dst        = NULL;
+	list_init(&self->link);
 }
