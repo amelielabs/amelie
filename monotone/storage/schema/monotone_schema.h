84e2b4f9630fa53bf8d62f36d57d20acc76359f8
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-23 15:53:45 +0300

    schema: add module

---
+++ monotone/storage/schema/monotone_schema.h
@@ -0,0 +1,13 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+#include "schema/type.h"
+#include "schema/column.h"
+#include "schema/schema.h"
+#include "schema/row.h"
+#include "schema/compare.h"
