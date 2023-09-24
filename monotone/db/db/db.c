4db326271de0130e1273a11ab6119977a57ae925
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-24 16:50:51 +0300

    db: add Db object

---
+++ monotone/db/db/db.c
@@ -0,0 +1,59 @@
+
+//
+// monotone
+//	
+// SQL OLTP database
+//
+
+#include <monotone_runtime.h>
+#include <monotone_io.h>
+#include <monotone_data.h>
+#include <monotone_lib.h>
+#include <monotone_config.h>
+#include <monotone_schema.h>
+#include <monotone_mvcc.h>
+#include <monotone_engine.h>
+#include <monotone_storage.h>
+#include <monotone_db.h>
+
+void
+db_init(Db* self, MetaIf* iface, void* arg)
+{
+	storage_mgr_init(&self->storage_mgr);
+	table_mgr_init(&self->table_mgr);
+	meta_mgr_init(&self->meta_mgr, iface, arg);
+}
+
+void
+db_free(Db* self)
+{
+	storage_mgr_free(&self->storage_mgr);
+	table_mgr_free(&self->table_mgr);
+	meta_mgr_free(&self->meta_mgr);
+}
+
+void
+db_open(Db* self, CatalogMgr* cat_mgr)
+{
+	// recover storages
+	storage_mgr_open(&self->storage_mgr);
+
+	// register catalog
+	auto cat = catalog_allocate("db", &db_catalog_if, self);
+	catalog_mgr_add(cat_mgr, cat);
+}
+
+void
+db_close(Db* self)
+{
+	// stop wal
+
+	// free meta
+	meta_mgr_free(&self->meta_mgr);
+
+	// free tables
+	table_mgr_free(&self->table_mgr);
+
+	// stop storages
+	storage_mgr_free(&self->storage_mgr);
+}
---
+++ monotone/db/db/db.h
@@ -0,0 +1,21 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Db Db;
+
+struct Db
+{
+	MetaMgr    meta_mgr;
+	TableMgr   table_mgr;
+	StorageMgr storage_mgr;
+};
+
+void db_init(Db*, MetaIf*, void*);
+void db_free(Db*);
+void db_open(Db*, CatalogMgr*);
+void db_close(Db*);
