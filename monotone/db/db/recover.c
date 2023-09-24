2c32d811a21ef32d06a77c8cd8b008a5f0fdee10
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-24 16:51:29 +0300

    db: add recover function wrapper

---
+++ monotone/db/db/recover.c
@@ -0,0 +1,24 @@
+
+//
+// monotone
+//	
+// SQL OLTP database
+//
+
+#include <monotone_runtime.h>
+#include <monotone_io.h>
+#include <monotone_data.h>
+#include <monotone_lib.h>
+#include <monotone_config.h>
+#include <monotone_schema.h>
+#include <monotone_mvcc.h>
+#include <monotone_engine.h>
+#include <monotone_storage.h>
+#include <monotone_db.h>
+
+void
+recover(Db* self)
+{
+	// todo
+	unused(self);
+}
---
+++ monotone/db/db/recover.h
@@ -0,0 +1,9 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+void recover(Db*);
