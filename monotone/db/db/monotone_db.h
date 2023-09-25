7921318c1876fddf512346b45501b197f8f86c57
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-25 13:36:11 +0300

    db: update module header

--- monotone/db/db/monotone_db.h
+++ monotone/db/db/monotone_db.h
@@ -10,14 +10,12 @@
 #include "db/table_config.h"
 #include "db/table.h"
 #include "db/table_op.h"
-#include "db/table_cache.h"
 #include "db/table_mgr.h"
 
 // meta
 #include "db/meta_config.h"
 #include "db/meta.h"
 #include "db/meta_op.h"
-#include "db/meta_cache.h"
 #include "db/meta_mgr.h"
 
 // db
