8c83294bb4eae1c70259db3f71aff209691440a4
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-26 17:55:13 +0300

    schema: use row_key for compare

--- monotone/storage/schema/compare.h
+++ monotone/storage/schema/compare.h
@@ -14,8 +14,8 @@ compare_n(Schema* schema, int count, Row* a, Row* b)
 	auto key = schema->key;
 	for (; key && count > 0; key = key->next_key)
 	{
-		uint8_t* a_data = row_key_of(a, schema, key->order_key);
-		uint8_t* b_data = row_key_of(b, schema, key->order_key);
+		uint8_t* a_data = row_key(a, schema, key->order_key);
+		uint8_t* b_data = row_key(b, schema, key->order_key);
 		int rc;
 		if (key->type == TYPE_STRING)
 			rc = data_compare_string(a_data, b_data);
@@ -32,10 +32,4 @@ hot static inline int
 compare(Schema* schema, Row* a, Row* b)
 {
 	return compare_n(schema, schema->key_count, a, b);
-}
-
-hot static inline bool
-row_compare(Schema* schema, Row *a, Row* b)
-{
-	return a->hash == b->hash && !compare(schema, a, b);
 }
