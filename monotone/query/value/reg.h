b96b870685730e87e041fe9805ea1a06a84eab0f
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-28 16:19:08 +0300

    value: add register to Value map

---
+++ monotone/query/value/reg.h
@@ -0,0 +1,34 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Reg Reg;
+
+struct Reg
+{
+	Value r[64];
+};
+
+static inline void
+reg_init(Reg* self)
+{
+	memset(self->r, 0, sizeof(self->r));
+}
+
+static inline void
+reg_reset(Reg* self)
+{
+	int i = 0;
+	for (; i < 64; i++)
+		value_free(&self->r[i]);
+}
+
+always_inline hot static inline Value*
+reg_at(Reg* self, int n)
+{
+	return &self->r[n];
+}
