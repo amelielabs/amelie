88b4ec89a9d6b8cf220558d341ad25025fa37bd5
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-28 16:21:55 +0300

    value: update module header

--- monotone/query/value/monotone_value.h
+++ monotone/query/value/monotone_value.h
@@ -6,3 +6,11 @@
 // SQL OLTP database
 //
 
+#include "value/value.h"
+#include "value/reg.h"
+#include "value/stack.h"
+#include "value/update.h"
+#include "value/map.h"
+#include "value/array.h"
+#include "value/row.h"
+#include "value/op.h"
