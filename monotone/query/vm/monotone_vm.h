b1fc721f69a9664a75653e97f98614a4da837d0e
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-29 13:22:43 +0300

    vm: update module header

--- monotone/query/vm/monotone_vm.h
+++ monotone/query/vm/monotone_vm.h
@@ -6,3 +6,20 @@
 // SQL OLTP database
 //
 
+// bytecode
+#include "vm/op.h"
+#include "vm/code.h"
+
+// functions
+#include "vm/function.h"
+#include "vm/function_mgr.h"
+
+// vm
+#include "vm/cursor.h"
+#include "vm/vm.h"
+#include "vm/cmd.h"
+#include "vm/func.h"
+#include "vm/func_debug.h"
+
+// callable
+#include "vm/callable.h"
