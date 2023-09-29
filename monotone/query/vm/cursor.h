efbe3f575a983561502b402d047ded87a1903ada
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-29 13:20:16 +0300

    vm: add cursor

---
+++ monotone/query/vm/cursor.h
@@ -0,0 +1,126 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Cursor    Cursor;
+typedef struct CursorMgr CursorMgr;
+
+enum
+{
+	CURSOR_NONE,
+	CURSOR_TABLE,
+	CURSOR_ARRAY,
+	CURSOR_SET,
+	CURSOR_GROUP
+};
+
+struct Cursor
+{
+	int       type;
+	int       r;
+	// storage
+	Table*    table;
+	Storage*  storage;
+	Index*    index;
+	Iterator* it;
+	// expr
+	uint8_t*  array_data;
+	int       array_pos;
+	int       array_count;
+	// set
+	Set*      set;
+	int       set_pos;
+	// group
+	Group*    group;
+	int       group_pos;
+	// limit/offset
+	int64_t   limit;
+	int64_t   offset;
+};
+
+struct CursorMgr
+{
+	Cursor cursor[16];
+};
+
+static inline void
+cursor_init(Cursor* self)
+{
+	self->type        = CURSOR_NONE;
+	self->r           = 0;
+	self->table       = NULL;
+	self->storage     = NULL;
+	self->index       = NULL;
+	self->it          = NULL;
+	self->array_data  = NULL;
+	self->array_pos   = 0;
+	self->array_count = 0;
+	self->set         = NULL;
+	self->set_pos     = 0;
+	self->group       = NULL;
+	self->group_pos   = 0;
+	self->limit       = 0;
+	self->offset      = 0;
+}
+
+static inline void
+cursor_reset(Cursor* self)
+{
+	self->type        = CURSOR_NONE;
+	self->r           = 0;
+	self->table       = NULL;
+	self->storage     = NULL;
+	self->index       = NULL;
+	self->array_data  = NULL;
+	self->array_pos   = 0;
+	self->array_count = 0;
+	self->set         = NULL;
+	self->set_pos     = 0;
+	self->group       = NULL;
+	self->group_pos   = 0;
+	self->limit       = 0;
+	self->offset      = 0;
+	if (self->it)
+	{
+		iterator_close(self->it);
+		iterator_free(self->it);
+		self->it = NULL;
+	}
+}
+
+static inline void
+cursor_mgr_init(CursorMgr* self)
+{
+	for (int i = 0; i < 16; i++)
+	{
+		auto cursor = &self->cursor[i];
+		cursor_init(cursor);
+	}
+}
+
+static inline void
+cursor_mgr_reset(CursorMgr* self)
+{
+	for (int i = 0; i < 16; i++)
+	{
+		auto cursor = &self->cursor[i];
+		cursor_reset(cursor);
+	}
+}
+
+static inline void
+cursor_mgr_free(CursorMgr* self)
+{
+	cursor_mgr_reset(self);
+}
+
+static inline Cursor*
+cursor_mgr_of(CursorMgr* self, int id)
+{
+	assert(id < 16);
+	return &self->cursor[id];
+}
