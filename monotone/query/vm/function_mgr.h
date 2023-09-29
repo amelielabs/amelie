d5fe16077532ea6fdba3c6055c7c317d15e6cc0d
Dmitry Simonenko <pmwkaa@gmail.com>
2023-09-29 13:20:03 +0300

    vm: add function manager

---
+++ monotone/query/vm/function_mgr.h
@@ -0,0 +1,50 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct FunctionMgr FunctionMgr;
+
+struct FunctionMgr
+{
+	List list;
+	int  list_count;
+};
+
+static inline void
+function_mgr_init(FunctionMgr* self)
+{
+	self->list_count = 0;
+	list_init(&self->list);
+}
+
+static inline void
+function_mgr_free(FunctionMgr* self)
+{
+	list_foreach_safe(&self->list) {
+		auto func = list_at(Function, link);
+		function_free(func);
+	}
+}
+
+static inline void
+function_mgr_add(FunctionMgr* self, Function* func)
+{
+	list_append(&self->list, &func->link);
+	self->list_count++;
+}
+
+static inline Function*
+function_mgr_find(FunctionMgr* self, Str* name)
+{
+	list_foreach(&self->list)
+	{
+		auto func = list_at(Function, link);
+		if (str_compare(&func->name, name))
+			return func;
+	}
+	return NULL;
+}
