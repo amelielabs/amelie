44c35511654496fda75a8eb68141adb92ae4e0d5
Dmitry Simonenko <pmwkaa@gmail.com>
2023-10-04 16:27:12 +0300

    parser: introduce parser module

---
+++ monotone/query/parser/monotone_parser.h
@@ -0,0 +1,26 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+// tokens and keywords
+#include "parser/keywords.h"
+
+// ast
+#include "parser/cardinality.h"
+#include "parser/ast.h"
+#include "parser/ast_list.h"
+#include "parser/ast_stack.h"
+
+// query
+#include "parser/stmt.h"
+#include "parser/query.h"
+
+// lex
+#include "parser/lex.h"
+
+// parser
+#include "parser/parser.h"
