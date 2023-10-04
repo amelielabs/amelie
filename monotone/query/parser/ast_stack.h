0f1b453609c787b3bc1b75a1df710548ee01adb7
Dmitry Simonenko <pmwkaa@gmail.com>
2023-10-04 16:28:31 +0300

    parser: add ast stack

---
+++ monotone/query/parser/ast_stack.h
@@ -0,0 +1,53 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct AstStack AstStack;
+
+struct AstStack
+{
+	Ast* list;
+	Ast* list_tail;
+};
+
+static inline void
+ast_stack_init(AstStack* self)
+{
+	self->list = NULL;
+	self->list_tail = NULL;
+}
+
+static inline Ast*
+ast_tail(AstStack* self)
+{
+	return self->list_tail;
+}
+
+static inline void
+ast_push(AstStack* self, Ast* ast)
+{
+	if (self->list == NULL)
+		self->list = ast;
+	else
+		self->list_tail->next = ast;
+	ast->prev = self->list_tail;
+	self->list_tail = ast;
+}
+
+static inline Ast*
+ast_pop(AstStack* self)
+{
+	if (unlikely(self->list_tail == NULL))
+		return NULL;
+	auto head = self->list_tail;
+	self->list_tail = head->prev;
+	if (self->list_tail == NULL)
+		self->list = NULL;
+	else
+		self->list_tail->next = NULL;
+	return head;
+}
