d87f8668a791a9155a94a878ee721aba1006d318
Dmitry Simonenko <pmwkaa@gmail.com>
2023-10-04 16:28:08 +0300

    parser: add ast

---
+++ monotone/query/parser/ast.h
@@ -0,0 +1,54 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Ast Ast;
+
+typedef enum
+{
+	AST_UNDEF,
+	AST_SIMPLE,    // pre-computable or const
+	AST_COMPLEX,   // name + simple_expr
+	AST_NAME,      // name
+	AST_OP,        // name > simple_expr
+	AST_CONDITION  // AND, OR, NOT (1 and 1 == simple)
+} AstType;
+
+struct Ast
+{
+	int id;
+	union {
+		uint64_t integer;
+		float    fp;
+		struct {
+			Str  string;
+			bool string_escape;
+		};
+	};
+	AstType type;
+	Ast*    next;
+	Ast*    prev;
+};
+
+static inline void*
+ast_allocate(int id, int size)
+{
+	assert(size >= (int)sizeof(Ast));
+	Ast* self = palloc(size);
+	memset(self, 0, sizeof(*self));
+	self->id   = id;
+	self->next = NULL;
+	self->prev = NULL;
+	return self;
+}
+
+static inline Ast*
+ast(int id)
+{
+	Ast* self = ast_allocate(id, sizeof(Ast));
+	return self;
+}
