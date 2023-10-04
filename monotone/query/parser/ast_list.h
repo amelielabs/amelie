b8802d663940e4763bdb28025da78c952f6e4368
Dmitry Simonenko <pmwkaa@gmail.com>
2023-10-04 16:28:17 +0300

    parser: add ast_list

---
+++ monotone/query/parser/ast_list.h
@@ -0,0 +1,61 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct AstListNode AstListNode;
+typedef struct AstList     AstList;
+
+struct AstListNode
+{
+	Ast*         ast;
+	AstListNode* next;
+};
+
+struct AstList
+{
+	AstListNode* list;
+	AstListNode* list_tail;
+	int          count;
+};
+
+static inline void
+ast_list_init(AstList* self)
+{
+	self->list      = NULL;
+	self->list_tail = NULL;
+	self->count     = 0;
+}
+
+static inline Ast*
+ast_list_last(AstList* self)
+{
+	assert(self->list_tail);
+	return self->list_tail->ast;
+}
+
+static inline void
+ast_list_add(AstList* self, Ast* ast)
+{
+	AstListNode* node;
+	node = palloc(sizeof(AstListNode));
+	node->ast  = ast;
+	node->next = NULL;
+	if (self->list == NULL)
+		self->list = node;
+	else
+		self->list_tail->next = node;
+	self->list_tail = node;
+	self->count++;
+}
+
+static inline void
+ast_list_restore(AstList* self, AstList* pos)
+{
+	*self = *pos;
+	if (self->list_tail)
+		self->list_tail->next = NULL;
+}
