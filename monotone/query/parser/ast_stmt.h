d4182f683fcce7aae0005bc9a20ccdc350cbdb13
Dmitry Simonenko <pmwkaa@gmail.com>
2023-10-06 18:23:42 +0300

    parser: make ast_stmt return actual types

--- monotone/query/parser/ast_stmt.h
+++ monotone/query/parser/ast_stmt.h
@@ -59,6 +59,7 @@ struct AstDropTable
 struct AstInsert
 {
 	Ast  ast;
+	bool unique;
 	Ast* table;
 	Ast* rows;
 	int  rows_count;
@@ -77,12 +78,12 @@ ast_show_of(Ast* ast)
 	return (AstShow*)ast;
 }
 
-static inline Ast*
+static inline AstShow*
 ast_show_allocate(Ast* expr)
 {
 	AstShow* self = ast_allocate(KSHOW, sizeof(AstShow));
 	self->expr = expr;
-	return &self->ast;
+	return self;
 }
 
 // set
@@ -92,13 +93,13 @@ ast_set_of(Ast* ast)
 	return (AstSet*)ast;
 }
 
-static inline Ast*
+static inline AstSet*
 ast_set_allocate(Ast* name, Ast* value)
 {
 	AstSet* self = ast_allocate(KSET, sizeof(AstSet));
 	self->name  = name;
 	self->value = value;
-	return &self->ast;
+	return self;
 }
 
 // create user
@@ -108,14 +109,14 @@ ast_create_user_of(Ast* ast)
 	return (AstCreateUser*)ast;
 }
 
-static inline Ast*
+static inline AstCreateUser*
 ast_create_user_allocate(void)
 {
 	AstCreateUser* self;
 	self = ast_allocate(KCREATE_USER, sizeof(AstCreateUser));
 	self->if_not_exists = false;
 	self->config        = NULL;
-	return &self->ast;
+	return self;
 }
 
 // create table
@@ -125,14 +126,14 @@ ast_create_table_of(Ast* ast)
 	return (AstCreateTable*)ast;
 }
 
-static inline Ast*
+static inline AstCreateTable*
 ast_create_table_allocate(void)
 {
 	AstCreateTable* self;
 	self = ast_allocate(KCREATE_TABLE, sizeof(AstCreateTable));
 	self->if_not_exists = false;
 	self->config        = NULL;
-	return &self->ast;
+	return self;
 }
 
 // drop user
@@ -142,14 +143,14 @@ ast_drop_user_of(Ast* ast)
 	return (AstDropUser*)ast;
 }
 
-static inline Ast*
+static inline AstDropUser*
 ast_drop_user_allocate(void)
 {
 	AstDropUser* self;
 	self = ast_allocate(KDROP_USER, sizeof(AstDropUser));
 	self->if_exists = false;
 	self->name      = NULL;
-	return &self->ast;
+	return self;
 }
 
 // drop table
@@ -159,16 +160,17 @@ ast_drop_table_of(Ast* ast)
 	return (AstDropTable*)ast;
 }
 
-static inline Ast*
+static inline AstDropTable*
 ast_drop_table_allocate(void)
 {
 	AstDropTable* self;
 	self = ast_allocate(KDROP_TABLE, sizeof(AstDropTable));
 	self->if_exists = false;
 	self->name      = NULL;
-	return &self->ast;
+	return self;
 }
 
+#if 0
 // insert
 static inline AstInsert*
 ast_insert_of(Ast* ast)
@@ -181,6 +183,7 @@ ast_insert_allocate(void)
 {
 	AstInsert* self;
 	self = ast_allocate(KINSERT, sizeof(AstInsert));
+	self->unique     = false;
 	self->table      = NULL;
 	self->rows       = NULL;
 	self->rows_count = 0;
@@ -202,3 +205,4 @@ ast_select_allocate(void)
 	self->expr = NULL;
 	return &self->ast;
 }
+#endif
