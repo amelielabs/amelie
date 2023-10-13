461bc33d3cf6bb04d30c1183c1801b0fa6ff8165
Dmitry Simonenko <pmwkaa@gmail.com>
2023-10-13 18:18:00 +0300

    parser: update module header

--- monotone/query/parser/monotone_parser.h
+++ monotone/query/parser/monotone_parser.h
@@ -7,7 +7,7 @@
 //
 
 // tokens and keywords
-#include "parser/keywords.h"
+#include "parser/keyword.h"
 
 // ast
 #include "parser/ast.h"
@@ -17,12 +17,23 @@
 // lex
 #include "parser/lex.h"
 
-// query
-#include "parser/ast_op.h"
-#include "parser/ast_stmt.h"
-#include "parser/query.h"
+// target
+#include "parser/target.h"
+#include "parser/stmt.h"
 
 // parser
 #include "parser/parser.h"
-#include "parser/parser_row.h"
-#include "parser/parser_op.h"
+#include "parser/parse_expr.h"
+#include "parser/parse_misc.h"
+#include "parser/parse_config.h"
+#include "parser/parse_user.h"
+#include "parser/parse_table.h"
+#include "parser/parse_insert.h"
+#include "parser/parse_update.h"
+
+#include "parser/parse_from.h"
+#include "parser/parse_aggr.h"
+#include "parser/parse_order.h"
+#include "parser/parse_group.h"
+#include "parser/parse_select.h"
+#include "parser/parse.h"
