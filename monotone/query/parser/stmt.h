f7b4a0cf1279da52efe35dde9cf5e3a562de94ee
Dmitry Simonenko <pmwkaa@gmail.com>
2023-10-13 18:12:07 +0300

    parser: add statement object

---
+++ monotone/query/parser/stmt.h
@@ -0,0 +1,68 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Stmt Stmt;
+
+typedef enum
+{
+	STMT_SHOW,
+	STMT_SET,
+	STMT_CREATE_USER,
+	STMT_CREATE_TABLE,
+	STMT_DROP_USER,
+	STMT_DROP_TABLE,
+	STMT_ALTER_USER,
+	STMT_ALTER_TABLE,
+	STMT_INSERT,
+	STMT_UPDATE,
+	STMT_DELETE,
+	STMT_SELECT
+} StmtId;
+
+struct Stmt
+{
+	StmtId     id;
+	Ast*       ast;
+	AstList    aggr_list;
+	TargetList target_list;
+	Lex*       lex;
+	Db*        db;
+	List       link;
+};
+
+static inline Stmt*
+stmt_allocate(StmtId id, Db* db, Lex* lex)
+{
+	Stmt* self = palloc(sizeof(Stmt));
+	self->id  = id;
+	self->ast = NULL;
+	self->lex = lex;
+	self->db  = db;
+	ast_list_init(&self->aggr_list);
+	target_list_init(&self->target_list);
+	list_init(&self->link);
+	return self;
+}
+
+always_inline static inline Ast*
+stmt_next(Stmt* self)
+{
+	return lex_next(self->lex);
+}
+
+always_inline static inline Ast*
+stmt_if(Stmt* self, int id)
+{
+	return lex_if(self->lex, id);
+}
+
+always_inline static inline void
+stmt_push(Stmt* self, Ast* ast)
+{
+	lex_push(self->lex, ast);
+}
