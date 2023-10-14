0316348511644ab65eebdd841e3afca801b6c3cd
Dmitry Simonenko <pmwkaa@gmail.com>
2023-10-14 10:25:04 +0300

    parser: add UPDATE parse

--- monotone/query/parser/parse_update.c
+++ monotone/query/parser/parse_update.c
@@ -21,115 +21,6 @@
 #include <monotone_vm.h>
 #include <monotone_parser.h>
 
-#if 0
-static inline void
-stmt_update(Cmd* cmd)
-{
-	/* UPDATE name [SET|UNSET] path = expr [, ... ] [WHERE expr] [LIMIT expr] [OFFSET expr] */
-
-	/* table_name, expression or join */
-	int level = target_list_next_level(&cmd->target_list);
-
-	target_t *target;
-	target = stmt_from(cmd, level);
-	table_t *table = target->table;
-	if (table == NULL)
-		cmd_error(cmd, "UPDATE <table name> expected");
-	if (! table_is_primary(target->table))
-		cmd_error(cmd, "UPDATE table primary index expected");
-
-	ast_t *expr_update = NULL;
-	ast_t *expr_prev   = NULL;
-	int       count = 0;
-	for (;;)
-	{
-		/* [SET | UNSET] */
-		token_t op;
-		int rc;
-		rc = cmd_next(cmd, &op);
-		switch (rc) {
-		case KSET:
-		case KUNSET:
-			break;
-		default:
-			cmd_push(cmd, &op);
-			op.type = KSET;
-			break;
-		}
-
-		/* name, path or column id */
-		token_t path;
-		rc = cmd_next(cmd, &path);
-		switch (rc) {
-		case LEX_NAME:
-		case LEX_NAME_COMPOUND:
-		case LEX_INT:
-			break;
-		default:
-			cmd_error(cmd, "UPDATE name <name or pos> expected");
-		}
-
-		ast_t *expr = NULL;
-		if (op.type == KSET)
-		{
-			/* [SET] <path> [=] <expr> */
-
-			/* = */
-			cmd_try(cmd, NULL, '=');
-			
-			/* expr */
-			expr = expr_parse(cmd, NULL);
-		}
-
-		/* expr */
-		/* op(path, expr) */
-		ast_t *path_expr = ast(cmd, &path, NULL, NULL);
-		ast_t *op_expr = ast(cmd, &op, path_expr, expr);
-		if (expr_update == NULL)
-			expr_update = op_expr;
-		else
-			expr_prev->next = op_expr;
-		expr_prev = op_expr;
-		count++;
-
-		/* , */
-		rc = cmd_try(cmd, NULL, ',');
-		if (! rc)
-			break;
-	}
-
-	/* [WHERE] */
-	ast_t *expr_where = NULL;
-	if (cmd_try(cmd, NULL, KWHERE))
-		expr_where = expr_parse(cmd, NULL);
-
-	/* combine join on and where expression */
-	expr_where = stmt_from_joon_and_where(cmd, target, expr_where);
-
-	/* [LIMIT] */
-	ast_t *expr_limit = NULL;
-	if (cmd_try(cmd, NULL, KLIMIT))
-		expr_limit = expr_parse(cmd, NULL);
-
-	/* [OFFSET] */
-	ast_t *expr_offset = NULL;
-	if (cmd_try(cmd, NULL, KOFFSET))
-		expr_offset = expr_parse(cmd, NULL);
-
-	/* scan */
-	update_t update =
-	{
-		.target      = target,
-		.expr_update = expr_update,
-		.table       = table
-	};
-	scan(cmd, target, expr_limit, expr_offset, expr_where,
-	        NULL,
-	        stmt_update_on_match, &update);
-}
-
-#endif
-
 hot void
 parse_update(Stmt* self)
 {
@@ -140,4 +31,78 @@ parse_update(Stmt* self)
 	auto stmt = ast_update_allocate();
 	self->ast = &stmt->ast;
 
+	// table_name, expression or join
+	int level = target_list_next_level(&self->target_list);
+	stmt->target = parse_from(self, level);
+	stmt->table = stmt->target->table;
+	if (stmt->table == NULL)
+		error("UPDATE <table name> expected");
+
+	// todo: check primary index
+
+	Ast* expr_prev = NULL;
+	for (;;)
+	{
+		// [SET | UNSET]
+		auto op = stmt_next(self);
+		switch (op->id) {
+		case KSET:
+		case KUNSET:
+			break;
+		default:
+			error("UPDATE <SET|UNSET> expected");
+			break;
+		}
+
+		// name, path or column id
+		op->l = stmt_next(self);
+		switch (op->l->id) {
+		case KNAME:
+		case KNAME_COMPOUND:
+		case KINT:
+			break;
+		default:
+			error("UPDATE name <name or pos> expected");
+			break;
+		}
+
+		// expr
+		if (op->id == KSET)
+		{
+			// [SET] <path> [=] <expr>
+
+			// =
+			stmt_if(self, '=');
+
+			// expr
+			op->r = parse_expr(self, NULL);
+		}
+
+		// op(path, expr)
+		if (stmt->expr_update == NULL)
+			stmt->expr_update = op;
+		else
+			expr_prev->next = op;
+		expr_prev = op;
+
+		// ,
+		if(! stmt_if(self, ','))
+			break;
+	}
+
+	// [WHERE]
+	if (stmt_if(self, KWHERE))
+		stmt->expr_where = parse_expr(self, NULL);
+
+	// combine join on and where expression
+	stmt->expr_where =
+		parse_from_join_on_and_where(stmt->target, stmt->expr_where);
+
+	// [LIMIT]
+	if (stmt_if(self, KLIMIT))
+		stmt->expr_limit = parse_expr(self, NULL);
+
+	// [OFFSET]
+	if (stmt_if(self, KOFFSET))
+		stmt->expr_offset = parse_expr(self, NULL);
 }
