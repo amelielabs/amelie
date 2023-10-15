84d817dc082a306d9002df39dce8087bf40e2a40
Dmitry Simonenko <pmwkaa@gmail.com>
2023-10-15 17:35:49 +0300

    compiler: support delete statement

---
+++ monotone/query/compiler/emit_delete.c
@@ -0,0 +1,48 @@
+
+//
+// monotone
+//	
+// SQL OLTP database
+//
+
+#include <monotone_runtime.h>
+#include <monotone_io.h>
+#include <monotone_data.h>
+#include <monotone_lib.h>
+#include <monotone_config.h>
+#include <monotone_auth.h>
+#include <monotone_schema.h>
+#include <monotone_transaction.h>
+#include <monotone_storage.h>
+#include <monotone_wal.h>
+#include <monotone_db.h>
+#include <monotone_value.h>
+#include <monotone_aggr.h>
+#include <monotone_vm.h>
+#include <monotone_parser.h>
+#include <monotone_compiler.h>
+
+static inline void
+emit_delete_on_match(Compiler* self, void *arg)
+{
+	Target* target = arg;
+	op1(self, CDELETE, target->id);
+}
+
+void
+emit_delete(Compiler* self, Ast* ast)
+{
+	// DELETE FROM name
+	// [WHERE expr]
+	// [LIMIT expr] [OFFSET expr]
+	auto delete = ast_delete_of(ast);
+
+	// delete by cursor
+	scan(self, delete->target,
+	     delete->expr_limit,
+	     delete->expr_offset,
+	     delete->expr_where,
+	     NULL,
+	     emit_delete_on_match,
+	     delete->target);
+}
