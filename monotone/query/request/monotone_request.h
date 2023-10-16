0d9b89488b71d56d42d2bbd81a343dc84dc6c687
Dmitry Simonenko <pmwkaa@gmail.com>
2023-10-16 12:59:22 +0300

    request: introduce req module

---
+++ monotone/query/request/monotone_request.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+// bytecode
+#include "request/code_data.h"
+#include "request/code.h"
+
+// request
+#include "request/req_lock.h"
+#include "request/req.h"
+#include "request/req_cache.h"
+#include "request/req_list.h"
+#include "request/req_set.h"
