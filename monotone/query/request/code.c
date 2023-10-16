fc2b81fa769e92bf4dbbd0391ab4e3e50067edc1
Dmitry Simonenko <pmwkaa@gmail.com>
2023-10-16 13:00:13 +0300

    request: move code

---
+++ monotone/query/request/code.c
@@ -0,0 +1,81 @@
+
+//
+// monotone
+//	
+// SQL OLTP database
+//
+
+#include <monotone_runtime.h>
+#include <monotone_io.h>
+#include <monotone_data.h>
+#include <monotone_lib.h>
+#include <monotone_config.h>
+#include <monotone_auth.h>
+#include <monotone_client.h>
+#include <monotone_server.h>
+#include <monotone_schema.h>
+#include <monotone_transaction.h>
+#include <monotone_storage.h>
+#include <monotone_wal.h>
+#include <monotone_db.h>
+#include <monotone_request.h>
+
+void
+code_save(Code* self, CodeData* data, Buf* buf)
+{
+	// map
+	encode_map(buf, 2);
+
+	// code 
+	encode_raw(buf, "code", 4);
+	encode_array(buf, code_count(self));
+
+	auto op  = (Op*)self->code.start;
+	auto end = (Op*)self->code.position;
+	for (; op < end; op++)
+	{
+		encode_array(buf, 5);
+		encode_integer(buf, op->op);
+		encode_integer(buf, op->a);
+		encode_integer(buf, op->b);
+		encode_integer(buf, op->c);
+		encode_integer(buf, op->d);
+	}
+
+	// data
+	encode_raw(buf, "data", 4);
+	encode_buf(buf, &data->data);
+}
+
+void
+code_load(Code* self, CodeData* data, uint8_t** pos)
+{
+	// map
+	int count;
+	data_read_map(pos, &count);
+
+	// code
+	data_skip(pos);
+	data_read_array(pos, &count);
+	for (int i = 0; i < count; i++)
+	{
+		// array
+		int _unused = 0;
+		data_read_array(pos, &_unused);
+
+		int64_t op, a, b, c, d;
+		data_read_integer(pos, &op);
+		data_read_integer(pos, &a);
+		data_read_integer(pos, &b);
+		data_read_integer(pos, &c);
+		data_read_integer(pos, &d);
+
+		code_add(self, op, a, b, c, d);
+	}
+
+	// data
+	data_skip(pos);
+	uint8_t* ptr = *pos;
+	data_skip(pos);
+	buf_write(&data->data, ptr, *pos - ptr);
+}
---
+++ monotone/query/request/code.h
@@ -0,0 +1,82 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Op   Op;
+typedef struct Code Code;
+
+struct Op
+{
+	uint8_t op;
+	int64_t a, b, c, d;
+};
+
+struct Code
+{
+	Buf code;
+};
+
+static inline void
+code_init(Code* self)
+{
+	buf_init(&self->code);
+}
+
+static inline void
+code_free(Code* self)
+{
+	buf_free(&self->code);
+}
+
+static inline void
+code_reset(Code* self)
+{
+	buf_reset(&self->code);
+}
+
+static inline void
+code_copy(Code* self, Code* from)
+{
+	buf_write(&self->code, from->code.start, buf_size(&from->code));
+}
+
+static inline int
+code_count(Code* self)
+{
+	if (unlikely(self->code.start == NULL))
+		return 0;
+	return buf_size(&self->code) / sizeof(Op);
+}
+
+static inline Op*
+code_at(Code* self, int pos)
+{
+	assert(self->code.start != NULL);
+	return &((Op*)self->code.start)[pos];
+}
+
+hot static inline Op*
+code_add(Code*    self,
+         uint8_t  id,
+         uint64_t a,
+         uint64_t b,
+         uint64_t c,
+         uint64_t d)
+{
+	buf_reserve(&self->code, sizeof(Op));
+	auto op = (Op*)self->code.position;
+	op->op    = id;
+	op->a     = a;
+	op->b     = b;
+	op->c     = c;
+	op->d     = d;
+	buf_advance(&self->code, sizeof(Op));
+	return op;
+}
+
+void code_save(Code*, CodeData*, Buf*);
+void code_load(Code*, CodeData*, uint8_t**);
