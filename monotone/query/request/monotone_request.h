72752d6978ba856ce7e1c732be3be4c4b95466cc
Dmitry Simonenko <pmwkaa@gmail.com>
2023-10-16 14:42:28 +0300

    request: update module header

--- monotone/query/request/monotone_request.h
+++ monotone/query/request/monotone_request.h
@@ -12,8 +12,9 @@
 
 // request
 #include "request/req_lock.h"
+#include "request/req_map.h"
 #include "request/req.h"
 #include "request/req_cache.h"
 #include "request/req_list.h"
 #include "request/req_set.h"
-#include "request/req_map.h"
+#include "request/dispatch.h"
