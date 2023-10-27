c4175e5e7c1aabc67703d23a121bd11481b79862
Dmitry Simonenko <pmwkaa@gmail.com>
2023-10-27 17:32:25 +0300

    parser: add parse_target()

--- monotone/query/parser/parse_misc.h
+++ monotone/query/parser/parse_misc.h
@@ -27,3 +27,36 @@ parse_if_exists(Stmt* self)
 		error("IF <EXISTS> expected");
 	return true;
 }
+
+static inline Ast*
+parse_target(Stmt* self, Str* schema, Str* name)
+{
+	str_init(schema);
+	str_init(name);
+
+	// name
+	auto path = stmt_next(self);
+	if (path->id == KNAME)
+	{
+		str_set(schema, "public", 6);
+		str_set_str(name, &path->string);
+		return path;
+	}
+
+	// schema.name
+	if (path->id == KNAME_COMPOUND)
+	{
+		str_split_or_set(&path->string, schema, '.');
+		str_set_str(name, &path->string);
+		str_advance(name, str_size(schema) + 1);
+		if (strnchr(str_of(name), str_size(name), '.'))
+		{
+			stmt_push(self, path);
+			return NULL;
+		}
+		return path;
+	}
+
+	stmt_push(self, path);
+	return NULL;
+}
