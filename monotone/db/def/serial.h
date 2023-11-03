436c6f04fb16253a69db8cb5ec0aeba59d6ce946
Dmitry Simonenko <pmwkaa@gmail.com>
2023-11-03 17:09:46 +0200

    def: add Serial object

---
+++ monotone/db/def/serial.h
@@ -0,0 +1,32 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Serial Serial;
+
+struct Serial
+{
+	atomic_u64 value;
+};
+
+static inline void
+serial_init(Serial* self)
+{
+	self->value = 0;
+}
+
+static inline void
+serial_set(Serial* self, uint64_t value)
+{
+	atomic_u64_set(&self->value, value);
+}
+
+static inline uint64_t
+serial_next(Serial* self)
+{
+	return atomic_u64_inc(&self->value);
+}
