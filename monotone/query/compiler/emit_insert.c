c08a172826551439eb7018f92ddbe1a7f2c4bbe7
Dmitry Simonenko <pmwkaa@gmail.com>
2023-11-06 16:15:58 +0200

    compiler: remove former generate logic

--- monotone/query/compiler/emit_insert.c
+++ monotone/query/compiler/emit_insert.c
@@ -23,56 +23,13 @@
 #include <monotone_parser.h>
 #include <monotone_compiler.h>
 
-static atomic_u32 seq = 0;
-
-hot void
-emit_insert_generate(Compiler* self, AstInsert* insert)
-{
-	auto stmt = self->current;
-	int  count = insert->generate_count->integer;
-	for (int i = 0; i < count; i++)
-	{
-		int key = atomic_u32_inc(&seq);
-
-		// (batch)
-		int  data = code_data_pos(&self->code_data);
-		auto buf = &self->code_data.data;
-		int  count_batch = insert->generate_batch->integer;
-		encode_array(buf, count_batch);
-		for (int j = 0; j < count_batch; j++)
-			encode_integer(buf, key);
-		int data_size = buf_size(buf) - data;
-
-		// get the request code
-		uint32_t hash;
-		hash = hash_murmur3_32((uint8_t*)&key, sizeof(key), 0);
-
-		// get route by hash
-		auto route = router_get(self->router, hash);
-
-		// get the request code
-		auto code = &dispatch_add(self->dispatch, stmt->order, route)->code;
-
-		// CINSERT
-		code_add(code, CINSERT, (intptr_t)insert->target->table, data, data_size,
-		         insert->unique);
-	}
-}
-
 hot void
 emit_insert(Compiler* self, Ast* ast)
 {
 	auto stmt = self->current;
 	auto insert = ast_insert_of(ast);
 
-	// GENERATE
-	if (insert->generate)
-	{
-		emit_insert_generate(self, insert);
-		return;
-	}
-
-	// foreach row
+	// foreach row generate insert
 	auto def = table_def(insert->target->table);
 	auto i = insert->rows;
 	for (; i; i = i->next)
