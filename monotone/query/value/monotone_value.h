55b3a539a1466863d75c2bf52d4e01a60bab44bd
Dmitry Simonenko <pmwkaa@gmail.com>
2023-11-12 16:39:15 +0200

    value: add Result object

--- monotone/query/value/monotone_value.h
+++ monotone/query/value/monotone_value.h
@@ -6,11 +6,19 @@
 // SQL OLTP database
 //
 
+// value
 #include "value/value.h"
+
+// value objects
 #include "value/reg.h"
 #include "value/stack.h"
-#include "value/update.h"
+#include "value/result.h"
+
+// objects
 #include "value/map.h"
 #include "value/array.h"
+#include "value/update.h"
 #include "value/row.h"
+
+// operations
 #include "value/op.h"
---
+++ monotone/query/value/result.h
@@ -0,0 +1,58 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+typedef struct Result Result;
+
+struct Result
+{
+	Value* set;
+	int    set_size;
+	Buf    data;
+};
+
+static inline void
+result_init(Result* self)
+{
+	self->set      = NULL;
+	self->set_size = 0;
+	buf_init(&self->data);
+}
+
+static inline void
+result_reset(Result* self)
+{
+	for (int i = 0; i < self->set_size; i++)
+		value_free(&self->set[i]);
+	self->set      = NULL;
+	self->set_size = 0;
+	buf_reset(&self->data);
+}
+
+static inline void
+result_free(Result* self)
+{
+	result_reset(self);
+	buf_free(&self->data);
+}
+
+static inline void
+result_prepare(Result* self, int size)
+{
+	assert(! self->set);
+	int allocated = sizeof(Value) * size;
+	buf_reserve(&self->data, allocated);
+	self->set_size = size;
+	self->set      = (Value*)self->data.start;
+	memset(self->set, 0, allocated);
+}
+
+always_inline hot static inline Value*
+result_at(Result* self, int n)
+{
+	return &self->set[n];
+}
