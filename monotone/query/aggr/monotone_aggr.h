adcca5b960356de0808b6a5d109b33f9e0e19f1e
Dmitry Simonenko <pmwkaa@gmail.com>
2023-11-13 18:02:46 +0200

    aggr: update module header

--- monotone/query/aggr/monotone_aggr.h
+++ monotone/query/aggr/monotone_aggr.h
@@ -6,10 +6,17 @@
 // SQL OLTP database
 //
 
+// set
 #include "aggr/set.h"
+#include "aggr/set_iterator.h"
+#include "aggr/set_merge.h"
+
+// aggregate
 #include "aggr/aggr.h"
 #include "aggr/aggrs.h"
 #include "aggr/count.h"
 #include "aggr/sum.h"
 #include "aggr/avg.h"
+
+// group
 #include "aggr/group.h"
