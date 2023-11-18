0a888e70a607e451cb16dd3ce67a165dc42cdf81
Dmitry Simonenko <pmwkaa@gmail.com>
2023-11-18 16:09:12 +0200

    runtime: add compare_int64()

--- monotone/runtime/runtime/utils.h
+++ monotone/runtime/runtime/utils.h
@@ -15,3 +15,11 @@ strnchr(char* string, int size, int c)
 			return string;
 	return NULL;
 }
+
+hot static inline int
+compare_int64(int64_t a, int64_t b)
+{
+	if (a == b)
+		return 0;
+	return (a > b) ? 1 : -1;
+}
