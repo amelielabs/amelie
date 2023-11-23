ce92e8a34021487d1bcee36286455e5bdd185d97
Dmitry Simonenko <pmwkaa@gmail.com>
2023-11-23 16:46:12 +0200

    snapshot: add module header

---
+++ monotone/db/snapshot/monotone_snapshot.h
@@ -0,0 +1,13 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+#include "snapshot/snapshot_file.h"
+#include "snapshot/snapshot_cursor.h"
+#include "snapshot/snapshot_mgr.h"
+#include "snapshot/snapshot_batch.h"
+#include "snapshot/snapshot.h"
