b4391379486e1140d437a276f27286468205eec2
Dmitry Simonenko <pmwkaa@gmail.com>
2023-11-29 15:53:04 +0200

    storage: introduce storage module

---
+++ monotone/storage/storage/monotone_storage.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//
+// monotone
+//
+// SQL OLTP database
+//
+
+// partition
+#include "storage/part.h"
+#include "storage/part_tree.h"
+
+// storage
+#include "storage/storage_map.h"
+#include "storage/storage_config.h"
+#include "storage/storage.h"
+#include "storage/storage_iterator.h"
+#include "storage/storage_mgr.h"
