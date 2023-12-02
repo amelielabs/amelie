afb1529f8bda147a1f847ccfcb40d425f87ae11a
Dmitry Simonenko <pmwkaa@gmail.com>
2023-12-02 19:43:50 +0200

    system: remove RPC_USER related cases

--- monotone/main/system/system.c
+++ monotone/main/system/system.c
@@ -290,31 +290,6 @@ system_rpc(Rpc* rpc, void* arg)
 		system_ctl(self, session, stmt);
 		break;
 	}
-	/*
-	case RPC_USER_CREATE:
-	{
-		UserConfig* config = rpc_arg_ptr(rpc, 0);
-		bool if_not_exists = rpc_arg(rpc, 1);
-		user_mgr_create(&self->user_mgr, config, if_not_exists);
-		hub_mgr_sync_user_cache(&self->hub_mgr, &self->user_mgr.cache);
-		break;
-	}
-	case RPC_USER_DROP:
-	{
-		Str* name = rpc_arg_ptr(rpc, 0);
-		bool if_exists = rpc_arg(rpc, 1);
-		user_mgr_drop(&self->user_mgr, name, if_exists);
-		hub_mgr_sync_user_cache(&self->hub_mgr, &self->user_mgr.cache);
-		break;
-	}
-	case RPC_USER_ALTER:
-	{
-		UserConfig* config = rpc_arg_ptr(rpc, 0);
-		user_mgr_alter(&self->user_mgr, config);
-		hub_mgr_sync_user_cache(&self->hub_mgr, &self->user_mgr.cache);
-		break;
-	}
-	*/
 	case RPC_USER_SHOW:
 	{
 		Buf** buf = rpc_arg_ptr(rpc, 0);
