c231fee961df58753728b586ba8553300540edf0
Dmitry Simonenko <pmwkaa@gmail.com>
2025-03-26 12:34:43 +0200

    executor: temporary remove impl, prepare for rewrite

--- amelie/vm/executor/executor.h
+++ amelie/vm/executor/executor.h
@@ -11,20 +11,3 @@
 // AGPL-3.0 Licensed.
 //
 
-typedef struct Executor Executor;
-
-struct Executor
-{
-	Spinlock lock;
-	int      list_count;
-	List     list;
-	Commit   commit;
-	Router*  router;
-	Db*      db;
-};
-
-void executor_init(Executor*, Db*, Router*);
-void executor_free(Executor*);
-void executor_send(Executor*, Dtr*, int, ReqList*);
-void executor_recv(Executor*, Dtr*, int);
-void executor_commit(Executor*, Dtr*, Buf*);
