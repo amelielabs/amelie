aa43b38dfa68c0eb476934cf581704f9977b3588
Dmitry Simonenko <pmwkaa@gmail.com>
2025-03-31 12:31:55 +0300

    executor: temporary remove impl, prepare for rewrite

--- amelie/vm/executor/amelie_executor.h
+++ amelie/vm/executor/amelie_executor.h
@@ -15,21 +15,5 @@
 #include "executor/code_data.h"
 #include "executor/code.h"
 #include "executor/program.h"
-
-// request
 #include "executor/error.h"
-#include "executor/req.h"
-#include "executor/req_list.h"
-#include "executor/req_cache.h"
-#include "executor/req_queue.h"
 
-// pipe
-#include "executor/pipe.h"
-#include "executor/pipe_cache.h"
-#include "executor/pipe_set.h"
-
-// executor
-#include "executor/dispatch.h"
-#include "executor/dtr.h"
-#include "executor/commit.h"
-#include "executor/executor.h"
