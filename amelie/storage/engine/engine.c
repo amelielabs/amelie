0a1f34e0e3dd4611601f4a606fc0139c06e47ff1
Dmitry Simonenko <pmwkaa@gmail.com>
2025-12-29 19:02:03 +0200

    engine: add initial engine module

---
+++ amelie/storage/engine/engine.c
@@ -0,0 +1,16 @@
+
+//
+// amelie.
+//
+// Real-Time SQL OLTP Database.
+//
+// Copyright (c) 2024 Dmitry Simonenko.
+// Copyright (c) 2024 Amelie Labs.
+//
+// AGPL-3.0 Licensed.
+//
+
+#include <amelie_runtime>
+#include <amelie_partition>
+#include <amelie_wal.h>
+#include <amelie_engine.h>
