29918636d2130a205de998357433c1d9bd0bcaae
Dmitry Simonenko <pmwkaa@gmail.com>
2026-03-16 17:03:05 +0200

    part: add snapshot to branch

--- amelie/db/part/branch.h
+++ amelie/db/part/branch.h
@@ -15,8 +15,9 @@ typedef struct Branch Branch;
 
 struct Branch
 {
-	int64_t id;
 	Str     name;
+	int64_t id;
+	int64_t snapshot;
 	List    link;
 };
 
@@ -25,6 +26,7 @@ branch_allocate(void)
 {
 	auto self = (Branch*)am_malloc(sizeof(Branch));
 	self->id = 0;
+	self->snapshot = 0;
 	str_init(&self->name);
 	list_init(&self->link);
 	return self;
@@ -38,24 +40,31 @@ branch_free(Branch* self)
 }
 
 static inline void
-branch_set_id(Branch* self, uint64_t id)
-{
-	self->id = id;
-}
-
-static inline void
 branch_set_name(Branch* self, Str* name)
 {
 	str_free(&self->name);
 	str_copy(&self->name, name);
 }
 
+static inline void
+branch_set_id(Branch* self, uint64_t value)
+{
+	self->id = value;
+}
+
+static inline void
+branch_set_snapshot(Branch* self, uint64_t value)
+{
+	self->snapshot = value;
+}
+
 static inline Branch*
 branch_copy(Branch* from)
 {
 	auto self = branch_allocate();
-	branch_set_id(self, from->id);
 	branch_set_name(self, &from->name);
+	branch_set_id(self, from->id);
+	branch_set_snapshot(self, from->snapshot);
 	return self;
 }
 
@@ -66,9 +75,10 @@ branch_read(uint8_t** pos)
 	errdefer(branch_free, self);
 	Decode obj[] =
 	{
-		{ DECODE_INT,    "id",   &self->id   },
-		{ DECODE_STRING, "name", &self->name },
-		{ 0,              NULL,   NULL       },
+		{ DECODE_STRING, "name",     &self->name     },
+		{ DECODE_INT,    "id",       &self->id       },
+		{ DECODE_INT,    "snapshot", &self->snapshot },
+		{ 0,              NULL,       NULL           },
 	};
 	decode_obj(obj, "branch", pos);
 	return self;
@@ -82,13 +92,17 @@ branch_write(Branch* self, Buf* buf, int flags)
 	// map
 	encode_obj(buf);
 
+	// name
+	encode_raw(buf, "name", 4);
+	encode_string(buf, &self->name);
+
 	// id
 	encode_raw(buf, "id", 2);
 	encode_integer(buf, self->id);
 
-	// name
-	encode_raw(buf, "name", 4);
-	encode_string(buf, &self->name);
+	// snapshot
+	encode_raw(buf, "snapshot", 8);
+	encode_integer(buf, self->snapshot);
 
 	encode_obj_end(buf);
 }
