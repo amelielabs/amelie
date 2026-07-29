6c37401b3c5896fd98821965b3bca4a919d07396
Dmitry Simonenko <pmwkaa@gmail.com>
2026-07-29 13:22:11 +0400

    bench: rework upsert benchmark using function

--- amelie/main/bench/bench_upsert.c
+++ amelie/main/bench/bench_upsert.c
@@ -19,8 +19,29 @@ bench_upsert_create(Bench* self, Client* client)
 {
 	unused(self);
 	Str str;
-	str_set_cstr(&str, "create table test(id int primary key using hash as identity random (100000), data int default 0)");
+	str_set_cstr(&str, "create table test(id int primary key using hash as identity (100000), data int default 0)");
 	client_execute(client, &str, NULL);
+
+	auto batch = opt_int_of(&self->batch);
+	Buf buf;
+	buf_init(&buf);
+	defer_buf(&buf);
+	buf_format(&buf,
+	           "create function upsert() "
+	           "begin "
+	           "  insert into test () values ");
+	for (uint64_t i = 0; i < batch; i++)
+	{
+		buf_write(&buf, "()", 2);
+		if ((i + 1) != batch)
+			buf_write(&buf, ",", 1);
+	}
+	buf_format(&buf, "   on conflict do update set data = data + 1;");
+	buf_format(&buf, "end");
+
+	Str cmd;
+	buf_str(&buf, &cmd);
+	client_execute(client, &cmd, NULL);
 }
 
 hot static void
@@ -29,15 +50,8 @@ bench_upsert_main(BenchWorker* self, Client* client)
 	auto bench = self->bench;
 	auto batch = opt_int_of(&bench->batch);
 
-	char text[256];
-	format(text, sizeof(text),
-	       "insert into test "
-	       "generate {u64} "
-	       "on conflict do update set data = data + 1",
-	       batch);
 	Str cmd;
-	str_set_cstr(&cmd, text);
-
+	str_set_cstr(&cmd,  "execute upsert();");
 	while (! self->shutdown)
 	{
 		client_execute(client, &cmd, NULL);
